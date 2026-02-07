a4153549f9815361413f4e4df9a3811c69246ab1
Akshat Soni <3011514S@student.gla.ac.uk>
2026-02-07 23:05:27 +0000

    Chapter 9.6: initial login system and html page

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category
+from rango.models import Page
+from rango.models import UserProfile
 # Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -10,4 +12,5 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
 
