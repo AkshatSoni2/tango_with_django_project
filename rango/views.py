9fa8635872b7a8eac2182846e7ecddd2e8214677
Akshat Soni <3011514S@student.gla.ac.uk>
2026-02-09 21:16:38 +0000

    Chapter 10: exersises, tests passed

--- rango/views.py
+++ rango/views.py
@@ -21,11 +21,8 @@ def index(request):
     context_dict['pages'] = page_list
 
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
 
     response = render(request,  'rango/index.html', context=context_dict)
-
-    
     return response
 
 def about(request):
@@ -35,7 +32,10 @@ def about(request):
         print("TEST COOKIE WORKED!")
         request.session.delete_test_cookie()
 
-    return render(request, 'rango/about.html', {}) 
+    visitor_cookie_handler(request)
+    visits = request.session.get('visits', 0)
+    context_dict = {'visits': visits}
+    return render(request, 'rango/about.html', context=context_dict) 
 
 def show_category(request, category_name_slug):
     context_dict = {}
